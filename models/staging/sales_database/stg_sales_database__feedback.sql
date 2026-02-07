e24180a35184a3f85e4d0a2db65349ac2732590a
Baptiste Berthaud <baptisteberthaud@hotmail.fr>
2026-02-07 15:49:10 +0000

    fix

--- models/staging/sales_database/stg_sales_database__feedback.sql
+++ models/staging/sales_database/stg_sales_database__feedback.sql
@@ -1,6 +1,6 @@
 select
  CONCAT(feedback_id, '_', order_id) AS feedback_id,
  order_id,
- feedback_score as fs,
+ feedback_score,
  DATETIME(feedback_form_sent_date, "Europe/Paris") AS feedback_form_sent_at
 from {{ source('sales_database', 'feedback') }}
