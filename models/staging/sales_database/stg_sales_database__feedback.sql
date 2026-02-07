e5c25cb01947251bf5c7ef864e9ac1faedafbd37
Baptiste Berthaud <baptisteberthaud@hotmail.fr>
2026-02-07 15:53:48 +0000

    feat: test CI

--- models/staging/sales_database/stg_sales_database__feedback.sql
+++ models/staging/sales_database/stg_sales_database__feedback.sql
@@ -1,6 +1,6 @@
 select
  CONCAT(feedback_id, '_', order_id) AS feedback_id,
  order_id,
- feedback_score,
+ feedback_score as fs,
  DATETIME(feedback_form_sent_date, "Europe/Paris") AS feedback_form_sent_at
 from {{ source('sales_database', 'feedback') }}
